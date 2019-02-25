2c8dfc206f3d460fbe3d2179fda244db1c0147eb
Tegan <teganoar0@gmail.com>
2019-02-26 08:37:03 +1000

    updates if statements for beast mode 03_largest_number

--- 03_largest_number.rb
+++ 03_largest_number.rb
@@ -9,18 +9,20 @@
 
 
 def largest_number (number_1, number_2) #define method and allow to variables to be passed in.
-  numbers = [number_1, number_2] #create an array and allow the array to access variables.
-  if number_1.is_a? Numeric and number_2.is_a? Numeric #If statement to make sure arguments are numeric as a masterclass
-  puts numbers.max()
-  else 
-  puts "Invalid Response" #If the masterclas is not numeric, return that it is an invalid response to the user
+  if !number_1.is_a? Numeric or !number_2.is_a? Numeric #If statement to make sure arguments are numeric as a masterclass
+    puts "Give me a number"
+  elsif number_1 == number_2
+    puts "Your numbers are equal"
+  else
+    numbers = [number_1, number_2]
+    puts numbers.max()
   end
 end
 
-largest_number(100, 50)
+largest_number("hello",50)
 
 # Check your solution by filling in the folowing table:
-number_1 |100 | number_2 |50 | expected |100 | actual 100 
+#number_1 |100 | number_2 |50 | expected |100 | actual 100 
 
 # Beast Mode:
 # If you complete the challenge, feel free to come up
