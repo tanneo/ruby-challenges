73b5195a0951770c3535ad484743ff8159c19c53
Tegan <teganoar0@gmail.com>
2019-03-11 13:49:52 +1000

    completed 11_fizzbuzz

--- 11_fizzbuzz.rb
+++ 11_fizzbuzz.rb
@@ -26,4 +26,16 @@
 # Buzz
 # ...etc
 
-# Your code here
+i = 0
+while i < 100
+    i +=1
+    if 
+        i % 3 == 0
+        puts "Fizz"
+    elsif
+        i % 5 == 0
+        puts "FizzBuzz"   
+    else 
+        puts i
+    end
+end
