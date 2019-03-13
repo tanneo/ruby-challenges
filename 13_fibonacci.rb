81e8969cd84117d4bc9fa24bfcc6fdc738a60651
Tegan <teganoar0@gmail.com>
2019-03-13 10:30:18 +1000

    completed 13_fibonacci.rb

--- 13_fibonacci.rb
+++ 13_fibonacci.rb
@@ -11,6 +11,8 @@
 # Write the sequence on a piece of paper first and think about the steps
 # you take for each number. Translate this to pseudocode and then to ruby.
 
+#return index at nth number
+
 # Difficulty:
 # 6/10
 
@@ -22,6 +24,16 @@
 # Check your solution by running the tests:
 # ruby tests/13_fibonacci_test.rb
 
-def nthFibonacci (n)
-  # Your code here
-end
+def fibonacci(number)
+        a = 0
+        b = 1
+        number.times do
+            temp = a
+            a = b
+            # Add up previous two numbers in sequence.
+            b = temp + b
+        end
+        return a
+    end
+
+    puts fibonacci(10)
